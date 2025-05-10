710f2b5368522637981ee208b14add26c18a3580
ravneetk25 <ravneetkaur0725@gmail.com>
2025-05-10 23:51:27 +0530

    Resolved merge conflicts

---
+++ move_zeros.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int n= arr.size();
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0){
+                arr[count]=arr[i];
+                count++;
+            }
+            
+        }
+        while(count<n){
+                arr[count]=0;
+                count++;
+            }
+            //return arr[count];
+        
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
